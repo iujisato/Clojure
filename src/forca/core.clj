f8fc32405585c2c0d2c999dda7c93cf34f112727
iujisato <iuji.ujisato@ecostage.com.br>
2016-04-08 13:57:56 -0300

    Add core idea from hangman's game
    * Lesson 2 of Clojure course.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,15 @@
 (ns forca.core
   (:gen-class))
 
+(def total-chances 6)
+(defn you-lost [] (print "Awww...most there! You lost bro! =("))
+(defn game [remaining-chances]
+  (if (= remaining-chances 0)
+    (you-lost)
+    (game(dec remaining-chances 1))
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
