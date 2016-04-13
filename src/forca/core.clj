19bd97ce6e11fbdb652c51069e1cd23db56bf608
iujisato <iuji.ujisato@gmail.com>
2016-04-13 16:09:13 -0300

    Make main method start the game's core function
    * Lesson 8 of Clojure's course.
      * ps. Lesson 7 doesn't change the code, thats why the commit skip from
        lesson 6 to 8
    * Add start-game to main fucntion.
    * Now, you can start the file, not the fucntion, to play the clojure's hangman.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -2,31 +2,27 @@
   (:gen-class))
 
 (def total-chances 6)
+(def key-word "clojure")
 
 (defn you-lost [] (println "Awww...most there! You lost bro! =("))
 (defn you-win [] (println "Really? Gratz bro!!!"))
 
 (defn remaining-letters [word right-guesses]
-    (remove (fn [chosen-letter] (contains? right-guesses (str chosen-letter))) word)
-)
+    (remove (fn [chosen-letter] (contains? right-guesses (str chosen-letter))) word))
+
 (defn right-word? [word right-guesses]
-    (empty? (remaining-letters word right-guesses))
-)
+    (empty? (remaining-letters word right-guesses)))
 
 (defn contains-letter? [word guess]
-    (.contains word guess)
-)
+    (.contains word guess))
 
 (defn feedback-interface [remaining-chances word right-guesses]
-  (println "Remaining chances:" remaining-chances)
-  (doseq [character (seq word)]
+    (println "Remaining chances:" remaining-chances)
+    (doseq [character (seq word)]
     (if (contains? right-guesses (str character))
-      (print character " ")
-      (print "_" " ")
-    )
-  )
-  (println)
-)
+        (print character " ")
+        (print "_" " ")))
+    (println))
 
 (defn game [remaining-chances word right-guesses]
   (feedback-interface remaining-chances word right-guesses)
@@ -35,15 +31,17 @@
     (right-word? word right-guesses) (you-win)
     :else
     (let [guess (read-line)]
-      (if (contains-letter? word guess)
-        (do
-            (println "Whaaaat?! In da hole man!")
-            (recur remaining-chances word (conj right-guesses guess)))
-        (do
-            (println "Good one! You failed miserably rofl")
-            (recur (dec remaining-chances) word right-guesses))))) )
+        (if (contains-letter? word guess)
+            (do
+                (println "Whaaaat?! In da hole man!")
+                (recur remaining-chances word (conj right-guesses guess)))
+            (do
+                (println "You failed miserably rofl")
+                (recur (dec remaining-chances) word right-guesses))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn start-game [] (game total-chances key-word #{}))
+
+(defn -main [& args]
+    (println "Welcome to Clojure's hangman!")
+    (println "Please DO NOT use capslock on guesses")
+    (start-game))
